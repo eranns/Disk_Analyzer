c70dba46b01d5054f0facabb1380195f5435919a
eran.saranga@gmail.com <eran.saranga@gmail.com>
2020-10-04 19:17:55 +0300

    added makefile

---
+++ cmake-build-debug/CMakeFiles/Disk_Analyzer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Disk_Analyzer.dir/main.cpp.o"
+  "CMakeFiles/Disk_Analyzer.dir/src/FSFile.cpp.o"
+  "CMakeFiles/Disk_Analyzer.dir/src/FSthreadpool.cpp.o"
+  "Disk_Analyzer"
+  "Disk_Analyzer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Disk_Analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
